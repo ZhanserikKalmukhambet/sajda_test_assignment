d96059b7cd7e6606739dd5010a3343db84d4af1e
Zhanserik <zhanserik.kalmukhambet@gmail.com>
2024-02-14 01:01:42 +0600

    added third-party applications

--- main/settings.py
+++ main/settings.py
@@ -46,6 +46,8 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
+
+    'DEFAULT_PAGINATION_CLASS': 'web.pagination.StandardResultsSetPagination',
 }
 
 SIMPLE_JWT = {
